40f9b39b8ddf38d04da6c454ee43abc090e3dc7c
maikokko0317 <ukko_ukko@hotmail.com>
2018-12-06 14:13:37 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/saito/maiko/lesson6_taskapp/TaskApp.java
@@ -0,0 +1,13 @@
+package jp.techacademy.saito.maiko.lesson6_taskapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
